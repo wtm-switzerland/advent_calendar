e00a326d0df8a1f632f682a3408737e1751b7ed3
Katarina Sheremet <kate.romanowskaya@gmail.com>
2018-10-14 21:11:15 +0200

    Add list of wishes

--- lib/advent_view.dart
+++ lib/advent_view.dart
@@ -1,21 +1,40 @@
 import 'package:flutter/material.dart';
 
-class AdventViewWidget extends StatelessWidget {
+import 'christmas_wishes.dart';
 
+class AdventViewWidget extends StatelessWidget {
   final int adventNumber;
 
-  AdventViewWidget({@required this.adventNumber}) :
-        assert(adventNumber!=null), assert(adventNumber<25);
+  AdventViewWidget({@required this.adventNumber})
+      : assert(adventNumber != null),
+        assert(adventNumber < 25);
 
   @override
   Widget build(BuildContext context) => Scaffold(
-      appBar: AppBar(title: Text('Advent $adventNumber'),),
-      body: Image.asset(
-        'images/christmas-$adventNumber.jpg',
-        fit: BoxFit.cover,
-        height: double.infinity,
-        width: double.infinity,
-      ),
-    );
-
+        appBar: AppBar(
+          title: Text('Advent $adventNumber'),
+        ),
+        body: Stack(
+          alignment: const Alignment(0.0, 0.0),
+          children: <Widget>[
+            Image.asset(
+              'images/christmas-$adventNumber.jpg',
+              fit: BoxFit.cover,
+              height: double.infinity,
+              width: double.infinity,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                ChristmasWishes.wishesList[adventNumber-1],
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25.0,
+                    color: Colors.white),
+              ),
+            )
+          ],
+        ),
+      );
 }
---
+++ lib/christmas_wishes.dart
@@ -0,0 +1,29 @@
+class ChristmasWishes {
+  static const wishesList = <String>[
+    'Wishing you and your family a wonderful Holiday Season. With lots '
+        'of love from the WTM’s.',
+    'Wishing you a fun-filled Holiday season and best wishes for a Happy New Year!',
+    'To a healthy, happy, and peaceful New Year. Love from the WTM.',
+    'May the lights of Christmas be your guide and the Christmas carols fill you with cheer.',
+    'One of the nice things about Christmas is that you can make people forget the past with a present.',
+    'Christmas is a necessity. There has to be at least one day of the year to remind us that we\'re here for something else besides ourselves.',
+    'Wishing you Holidays filled with fun and laughter, and very best wishes for a fabulous New Year! ',
+    'Wishing you a wonderful Christmas Season creating special memories with your beautiful family.',
+    'Merry Christmas! And best wishes for a healthy, happy, and peaceful New Year.',
+    'Wishing you a joyous Christmas and a prosperous New Year.',
+    'Best wishes for the Holidays, and for health and happiness throughout the coming year.',
+    'Warmest wishes for a happy Christmas and a wonderful New Year.',
+    'Merry Christmas from all of us at [insert company name]. We hope you have a very happy Holidays and we wish you all the best for the New Year.',
+    'May this Christmas end the present year on a cheerful note and make way for a fresh and bright New Year. Here’s wishing you a Merry Christmas and a Happy New Year!',
+    'May the good times and treasures of the present become the golden memories of tomorrow. Wishing you lots of love, joy and happiness. Merry Christmas!',
+    'May your Christmas sparkle with moments of love, laughter and goodwill, And may the year ahead be full of contentment and joy. Have a Merry Christmas.',
+    'May the Christmas season fill your home with joy, your heart with love, and your life with laughter.',
+    'We should try to hold on to the Christmas spirit, not just one day a year, but all 365.',
+  '\“Christmas is most truly Christmas when we celebrate it by giving the light of love to those who need it most.\”—Ruth Carter Stapleton',
+    'May the peace and blessings of Christmas be yours; And may the coming year be filled with happiness.',
+    'May all the sweet magic of Christmas conspire to gladden your heart and fill every desire.',
+    'May the peace and joy of Christmas live in your heart all year long.',
+    'May the blessings of peace, good will, and happiness be with you at Christmas and always.',
+    'Me: \“I want a magical Unicorn for Christmas\”\nSanta: \“Be realistic\”\nMe: \“Okay. I want 5 minutes to myself each day to drink my coffee hot and pee in peace.\”\nSanta: “What color Unicorn would you like?\”'
+  ];
+}
