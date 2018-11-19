49ced2b475d5fd395a41606fee830296aba2f9b9
Katarina Sheremet <kate.romanowskaya@gmail.com>
2018-11-19 16:13:42 +0100

    Enable Firebase Cloud Messaging

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 import 'views/home_page_view.dart';
@@ -7,18 +8,25 @@ import 'views/home_page_view.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
   static final _analyticsNavigatorObserver =
       FirebaseAnalyticsObserver(analytics: FirebaseAnalytics());
 
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        title: 'Christmas Advent Calendar',
-        theme: ThemeData(
-          primarySwatch: Colors.indigo,
-        ),
-        home: const HomePageView(title: 'Christmas Advent Calendar'),
-        navigatorObservers: [
-          _analyticsNavigatorObserver,
-        ],
-      );
+  Widget build(BuildContext context) {
+    _firebaseMessaging
+      ..requestNotificationPermissions()
+      ..configure();
+
+    return MaterialApp(
+      title: 'Christmas Advent Calendar',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: const HomePageView(title: 'Christmas Advent Calendar'),
+      navigatorObservers: [
+        _analyticsNavigatorObserver,
+      ],
+    );
+  }
 }
